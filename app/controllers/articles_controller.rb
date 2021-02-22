4a8f65708cf0b29e98eaacc60a77a4312d3be20f
Carlos Villa <cvillad@uninorte.edu.co>
2021-02-22 15:28:33 -0500

    add user signup

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,5 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update, :destroy]
-    before_action :set_user, only: [:create, :update]
 
     def index
         @articles = Article.all
