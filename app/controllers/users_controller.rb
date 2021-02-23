64d9bc1dbc8a7ba3ceddacb6b3fe5f597f27aa6c
Carlos Villa <cvillad@uninorte.edu.co>
2021-02-23 17:31:55 -0500

    add user index, show and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,29 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def index
+        @users = User.all
+    end
+
+    def edit
+        @user = User.find(params[:id]) 
+    end
+
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "Account was successfully updated"
+            redirect_to articles_path
+        else
+            render :edit
+        end
+    end
+
     def create
         @user = User.new(user_params)
         if @user.save
