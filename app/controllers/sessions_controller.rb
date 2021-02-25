4731151a06d8c450c1cf79e53e968ee6c7690368
Carlos Villa <cvillad@uninorte.edu.co>
2021-02-25 11:12:36 -0500

    add sessions and pagination

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+    
+    def create
+        session_params = params[:session]
+        user = User.find_by(username: session_params[:username].downcase)
+        
+        if user && user.authenticate(session_params[:password])
+            session[:user_id] = user.id
+            flash[:notice] = "Logged in successfully"
+            redirect_to articles_path
+        else
+            flash.now[:alert] = "Check your username or password" 
+            render :new
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        flash[:notice] = "Logged out successfully"
+        redirect_to root_path
+    end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,25 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :show, :update]
 
     def new
         @user = User.new
     end
 
     def index
-        @users = User.all
+        @users = User.paginate(page: params[:page], per_page: 5)
     end
 
     def edit
-        @user = User.find(params[:id]) 
     end
 
     def show
-        @user = User.find(params[:id])
-        @articles = @user.articles
+        @articles = @user.articles.paginate(page: params[:page], per_page: 10)
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Account was successfully updated"
-            redirect_to articles_path
+            redirect_to @user
         else
             render :edit
         end
@@ -30,6 +28,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Hello #{@user.username}, you have sign up successfully"
             redirect_to articles_path
         else
@@ -41,6 +40,10 @@ class UsersController < ApplicationController
 
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 
 end
