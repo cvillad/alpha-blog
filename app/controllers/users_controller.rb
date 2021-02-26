ebe258a711f5503f3a124c8e0baf4f0b5ac2234c
Carlos Villa <cvillad@uninorte.edu.co>
2021-02-26 10:37:10 -0500

    add delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:edit, :show, :update]
-    before_action :require_user, only: [:edit, :update]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :set_user, only: [:edit, :show, :update, :destroy]
+    before_action :require_user, only: [:edit, :update, :destroy]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def new
         @user = User.new
@@ -25,6 +25,13 @@ class UsersController < ApplicationController
         else
             render :edit
         end
+    end
+
+    def destroy
+      @user.destroy
+      session[:user_id] = nil
+      flash[:notice] = "Account and articles successfully deleted"
+      redirect_to articles_path
     end
 
     def create
