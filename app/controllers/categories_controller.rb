7a9d3167083f241b33b5d90aaa00f1b9d422e7a4
Carlos Villa <cvillad@uninorte.edu.co>
2021-03-01 10:32:50 -0500

    add create category action and test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,27 @@
 class CategoriesController < ApplicationController
-
   def index 
   end
 
   def new 
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was succesfuly created"
+      redirect_to @category
+    else
+      render :new
+    end
   end
 
   def show 
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
   
 end 
